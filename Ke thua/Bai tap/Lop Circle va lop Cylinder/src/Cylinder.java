fb2208a2f350fc58c1beecb61fdc19336937e293
quy <you@example.com>
2019-10-16 11:30:40 +0700

    Lop Circle va Cylinder

---
+++ Ke thua/Bai tap/Lop Circle va lop Cylinder/src/Cylinder.java
@@ -0,0 +1,25 @@
+public class Cylinder extends Circle{
+    double height;
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume(){
+        return this.getArea()*this.height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "height=" + height +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}' +"Volume:" + this.getVolume();
+    }
+}
---
+++ Ke thua/Bai tap/Lop Circle va lop Cylinder/src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3, "red");
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(3,"blue",6);
+        System.out.println(cylinder);
+    }
+
+}
